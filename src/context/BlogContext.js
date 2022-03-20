1334b0b2c2be0e0e9caa3f26327d4854fe50a62f
Audry Wolters <audrywolters@gmail.com>
2022-03-20 15:27:42 -0500

    added helper function that adds special state data to context. it is in effect as adding a blog post on IndexScreen.

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const BlogContext = React.createContext()
 
 export const BlogProvider = ({ children }) => {
-	const blogPosts = [{ title: 'blog #1' }, { title: 'blog #2' }]
+	const [blogPosts, setBlogPosts] = useState([])
+
+	const addBlogPost = () => {
+		setBlogPosts([
+			...blogPosts,
+			{ title: `Blog Post #${blogPosts.length + 1}` }
+		])
+	}
 
 	// value is what is sent all around the app
-	return <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>
+	return (
+		<BlogContext.Provider value={{ data: blogPosts, addBlogPost: addBlogPost }}>
+			{children}
+		</BlogContext.Provider>
+	)
 }
 
 export default BlogContext
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,19 +1,21 @@
 import React, { useContext } from 'react'
-import { View, Text, StyleSheet, FlatList } from 'react-native'
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native'
 import BlogContext from '../context/BlogContext'
 
 const IndexScreen = () => {
-	const blogPosts = useContext(BlogContext)
+	const { data, addBlogPost } = useContext(BlogContext)
 
 	return (
 		<View>
 			<Text>IndexScreen</Text>
-			<FlatList 
-				data={blogPosts} 
-				keyExtractor={(blogPost) => blogPost.title} 
-				renderItem={ ({item}) => {
+			<Button title="Add Post" onPress={addBlogPost}/> 
+								 {/* onPress={() => addBlogPost()}/> */}
+			<FlatList
+				data={data}
+				keyExtractor={(blogPost) => blogPost.title}
+				renderItem={({ item }) => {
 					return <Text>{item.title}</Text>
-				}}	
+				}}
 			/>
 		</View>
 	)
