d11d58149cf3c010491a16e40170aa02ffcec6c3
Audry Wolters <audrywolters@gmail.com>
2022-03-20 17:20:25 -0500

    refactored BlogContext to have reusable Context/Provider/reducer thing. confusing

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,6 +1,5 @@
-import React, { useReducer } from 'react'
+import createDataContext from '../context/createDataContext'
 
-const BlogContext = React.createContext()
 const blogReducer = (state, action) => {
 	switch (action.type) {
 		case 'add_blogpost':
@@ -10,22 +9,14 @@ const blogReducer = (state, action) => {
 	}
 }
 
-export const BlogProvider = ({ children }) => {
-	// below is for learning stuff
-	//const [blogPosts, setBlogPosts] = useState([])
-	//    [state, dispatch]
-	const [blogPosts, dispatch] = useReducer(blogReducer, [])
-
-	const addBlogPost = () => {
+const addBlogPost = (dispatch) => {
+	return () => {
 		dispatch({ type: 'add_blogpost' })
 	}
-
-	// value is what is sent all around the app
-	return (
-		<BlogContext.Provider value={{ data: blogPosts, addBlogPost: addBlogPost }}>
-			{children}
-		</BlogContext.Provider>
-	)
 }
 
-export default BlogContext
+export const { Context, Provider } = createDataContext(
+	blogReducer,
+	{ addBlogPost },
+	[]
+)
