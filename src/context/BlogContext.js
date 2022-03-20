168b0eb793051787232683910fc30485ec9b502d
Audry Wolters <audrywolters@gmail.com>
2022-03-20 14:55:26 -0500

    experiementing with Context and Provider. Mini blogPosts sent.

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,8 +3,10 @@ import React from 'react'
 const BlogContext = React.createContext()
 
 export const BlogProvider = ({ children }) => {
+	const blogPosts = [{ title: 'blog #1' }, { title: 'blog #2' }]
+
 	// value is what is sent all around the app
-	return <BlogContext.Provider value={15}>{children}</BlogContext.Provider>
+	return <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>
 }
 
 export default BlogContext
