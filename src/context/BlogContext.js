cf070d184a35caea4e8abddb1d4d835341117712
Audry Wolters <audrywolters@gmail.com>
2022-03-20 14:44:57 -0500

    added useContext. Understanding provider betterer.

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,7 +3,8 @@ import React from 'react'
 const BlogContext = React.createContext()
 
 export const BlogProvider = ({ children }) => {
-	// AUDRY - keep an eye on 'value'
-	// tutorial hasn't gotten to it yet. gulp. could be outdated
-	return <BlogContext.Provider value="Blog">{children}</BlogContext.Provider>
+	// value is what is sent all around the app
+	return <BlogContext.Provider value={15}>{children}</BlogContext.Provider>
 }
+
+export default BlogContext
