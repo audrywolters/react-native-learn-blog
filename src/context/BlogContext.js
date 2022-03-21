e497a3d2b1508200ca7034400f9ea802ba1c0909
Audry Wolters <audrywolters@gmail.com>
2022-03-21 14:49:36 -0500

    added id to blogpost state. made trash icon tapable.

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,7 +3,13 @@ import createDataContext from '../context/createDataContext'
 const blogReducer = (state, action) => {
 	switch (action.type) {
 		case 'add_blogpost':
-			return [...state, { title: `Blogpost #${state.length + 1}` }]
+			return [
+				...state,
+				{
+					title: `Blogpost #${state.length + 1}`,
+					id: Math.floor(Math.random() * 99999)
+				}
+			]
 		default:
 			return state
 	}
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,15 +1,14 @@
 import React, { useContext } from 'react'
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native'
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native'
 import { Context } from '../context/BlogContext'
 import { Feather } from '@expo/vector-icons'
-import { TouchableOpacity } from 'react-native-gesture-handler'
 
 const IndexScreen = () => {
 	const { state, addBlogPost } = useContext(Context)
 
 	return (
 		<View>
-													{/* onPress={() => addBlogPost()}/> */}
+												{/* onPress={() => addBlogPost()}/> */}
 			<TouchableOpacity style={styles.button} onPress={addBlogPost}>
 				<Text style={styles.buttonText}>Add Post</Text>
 			</TouchableOpacity>
@@ -19,8 +18,10 @@ const IndexScreen = () => {
 				renderItem={({ item }) => {
 					return (
 						<View style={styles.row}>
-							<Text style={styles.title}>{item.title}</Text>
-							<Feather style={styles.icon} name="trash-2" />
+							<Text style={styles.title}>{item.title} - {item.id}</Text>
+							<TouchableOpacity onPress={() => console.log(item.id)}>
+								<Feather style={styles.icon} name="trash-2" />
+							</TouchableOpacity>
 						</View>
 					)
 				}}
