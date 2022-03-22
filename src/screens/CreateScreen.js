9f43e74781018e5ba05c96f88965bc703be1af5c
Audry Wolters <audrywolters@gmail.com>
2022-03-22 10:09:27 -0500

    made a new screen for Creating a new blog post. Isn't wired yet.

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { Context } from '../context/BlogContext'
+
+const CreateScreen = ({ navigation }) => {
+	return (
+		<View>
+			<Text>CreateScreen</Text>
+		</View>
+	)
+
+	// return {
+	// 	headerRight: () => (
+	// 	  <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+	// 		<Feather name="plus" size={30} />
+	// 	  </TouchableOpacity>
+	// 	),
+	//   };
+}
+
+const styles = StyleSheet.create({})
+
+export default CreateScreen
