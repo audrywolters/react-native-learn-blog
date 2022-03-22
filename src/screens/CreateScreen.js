d4da6cf984a35c5de4161b85306757013d3ff3f7
Audry Wolters <audrywolters@gmail.com>
2022-03-22 15:17:00 -0500

    added input and lables. added styling to all.

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,15 +1,68 @@
-import React, { useContext } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useContext, useState } from 'react'
+import {
+	View,
+	Text,
+	TextInput,
+	StyleSheet,
+	TouchableOpacity
+} from 'react-native'
 import { Context } from '../context/BlogContext'
 
 const CreateScreen = () => {
+	const [title, setTitle] = useState('')
+	const [content, setContent] = useState('')
+
 	return (
-		<View>
-			<Text>CreateScreen</Text>
+		<View style={styles.parent}>
+			<Text style={styles.label}>Enter Title:</Text>
+			<TextInput
+				style={styles.input}
+				value={title}
+				onChangeText={(text) => setTitle(text)}
+			/>
+			<Text style={styles.label}>Enter Content:</Text>
+			<TextInput
+				style={[styles.input, styles.contentInput]}
+				value={content}
+				onChangeText={(text) => setContent(text)}
+			/>
+			<TouchableOpacity style={styles.button} >
+				<Text style={styles.buttonText}>Save Blog Post</Text>
+			</TouchableOpacity>
 		</View>
 	)
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	parent: {
+		flex: 1,
+		margin: 8
+	},
+	label: {
+		fontSize: 18,
+		marginBottom: 5
+	},
+	input: {
+		fontSize: 16,
+		marginBottom: 20,
+		padding: 5,
+		height: 40,
+		borderWidth: 1,
+		borderColor: 'black'
+	},
+	contentInput: {
+		height: 500
+	},
+	button: {
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: 'black',
+		height: 55
+	},
+	buttonText: {
+		fontSize: 18,
+		color: 'white'
+	}
+})
 
 export default CreateScreen
